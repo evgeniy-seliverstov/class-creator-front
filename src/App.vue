51f54d29ee9ca72b120b076bc8895d27f5777a14
Eugeniy <serdsaerrr@mail.ru>
2020-05-16 16:54:18 +0500

    Added buttons without logic

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <v-app>
     <v-content>
+      <div class="ma-4">
+        <add-button />
+        <generate-button class="ml-4"/>
+      </div>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import AddButton from "@/components/Buttons/AddButton";
+import GenerateButton from "@/components/Buttons/GenerateButton";
+
 export default {
   name: 'App',
   data: () => ({
   }),
+  components: {
+    "add-button": AddButton,
+    "generate-button": GenerateButton
+  }
 };
 </script>
---
+++ src/components/Buttons/AddButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-btn>
+    <v-icon size="20" class="mr-2">mdi-plus-circle-outline</v-icon>
+    Добавить класс
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "AddButton"
+}
+</script>
