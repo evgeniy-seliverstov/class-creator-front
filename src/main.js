37d2baed9e0c755b3774090e29befcff379bb823
Eugeniy <serdsaerrr@mail.ru>
2020-05-17 18:39:55 +0500

    Added noty plugin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueNoty from 'vuejs-noty'
+import 'vuejs-noty/dist/vuejs-noty.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueNoty, {
+  theme: "bootstrap-v4",
+  timeout: 2000,
+})
 
 new Vue({
   store,
